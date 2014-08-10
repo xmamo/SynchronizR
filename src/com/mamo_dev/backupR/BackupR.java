a7fead149b196d96588a0520e8f23e504fe3886e
Matteo Morena <8678043+xmamo@users.noreply.github.com>
2014-08-10 17:09:58 +0200

    Updated languare packs ("??OK" -> "?? - OK", ...).

--- src/com/mamo_dev/backupR/BackupR.java
+++ src/com/mamo_dev/backupR/BackupR.java
@@ -20,8 +20,8 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 public class BackupR {
 
-	public static final String VERSION = "1.4.4";
-	public static final long releaseDate = 201408101703L;
+	public static final String VERSION = "1.4.5";
+	public static final long releaseDate = 201408101709L;
 
 	private static Properties properties;
 
