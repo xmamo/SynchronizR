2ca499e55524cbfd4c37ea65f64e4aad6f8bee5a
Matteo Morena <8678043+xmamo@users.noreply.github.com>
2014-08-10 17:25:44 +0200

    Corrected default window position.

--- src/com/mamo_dev/backupR/BackupR.java
+++ src/com/mamo_dev/backupR/BackupR.java
@@ -20,8 +20,8 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 public class BackupR {
 
-	public static final String VERSION = "1.4.6";
-	public static final long releaseDate = 201408101714L;
+	public static final String VERSION = "1.4.7";
+	public static final long releaseDate = 201408101725L;
 
 	private static Properties properties;
 
@@ -61,11 +61,6 @@ public class BackupR {
 				window.setVisible(true);
 				if (properties.getInt(PropertyEnum.WINDOW_X.toString()) == Integer.MIN_VALUE || properties.getInt(PropertyEnum.WINDOW_Y.toString()) == Integer.MIN_VALUE) {
 					window.setLocationRelativeTo(null);
-					try {
-						properties.set(PropertyEnum.WINDOW_X.toString(), (int) window.getLocationOnScreen().getX());
-						properties.set(PropertyEnum.WINDOW_Y.toString(), (int) window.getLocationOnScreen().getY());
-					} catch (IOException ex) {
-					}
 				} else {
 					window.setLocation(properties.getInt(PropertyEnum.WINDOW_X.toString()), properties.getInt(PropertyEnum.WINDOW_Y.toString()));
 				}
