3b5b5beff142935cc9f91926c8bc33a576e28acd
Matteo Morena <8678043+xmamo@users.noreply.github.com>
2014-08-10 17:20:55 +0200

    Translated "Done!", corrected a wrong translation in _de.

--- src/com/mamo_dev/backupR/BackupR.java
+++ src/com/mamo_dev/backupR/BackupR.java
@@ -20,8 +20,8 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 public class BackupR {
 
-	public static final String VERSION = "1.4.5";
-	public static final long releaseDate = 201408101709L;
+	public static final String VERSION = "1.4.6";
+	public static final long releaseDate = 201408101714L;
 
 	private static Properties properties;
 
