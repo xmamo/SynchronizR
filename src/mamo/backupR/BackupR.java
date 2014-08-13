bf3450a0aa2b8e2c4dbb3d80984575eaa07c3e76
Matteo Morena <8678043+xmamo@users.noreply.github.com>
2014-08-13 22:33:14 +0200

    Made EULA clearer.

--- src/mamo/backupR/BackupR.java
+++ src/mamo/backupR/BackupR.java
@@ -25,8 +25,8 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 public class BackupR {
 
-	public static final String VERSION = "1.7.5";
-	public static final long releaseDate = 201408131729L;
+	public static final String VERSION = "1.7.6";
+	public static final long releaseDate = 201408132222L;
 
 	private static Settings settings;
 	private static final Lang lang = new Lang("lang");
