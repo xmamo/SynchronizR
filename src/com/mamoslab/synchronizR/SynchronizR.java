b3f4522ff25606628a40a7f9e1df07141cb67333
Matteo Morena <8678043+xmamo@users.noreply.github.com>
2014-11-13 11:14:20 +0100

    Fixed little output log error.

--- src/com/mamoslab/synchronizR/SynchronizR.java
+++ src/com/mamoslab/synchronizR/SynchronizR.java
@@ -43,8 +43,8 @@ import javax.swing.UnsupportedLookAndFeelException;
 
 public class SynchronizR {
 
-	public static final String VERSION = "1.10";
-	public static final long releaseDate = 201411081311L;
+	public static final String VERSION = "1.10.1";
+	public static final long releaseDate = 201411131113L;
 
 	private static Settings settings;
 	private static final Lang lang = new Lang("lang");
